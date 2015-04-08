5d47699088defe8661c010d884b6a8eadaed62d8
selfcommit <oboyledk@gmail.com>
2015-04-08 10:55:24 -0400

    unit test

---
+++ lists/tests.py
@@ -0,0 +1,14 @@
+from django.test import TestCase
+from django.http import HttpRequest
+
+from lists.views import home_page
+
+# Create your tests here.
+class HomePageViewTest(TestCase):
+
+	def test_home_page_returns_correct_html(self):
+		request = HttpRequest()
+		response = home_page(request)
+		self.assertTrue(response.content.startswith(b'<html>'))
+		self.assertIn('<title>To-Do lists</title>', response.content.decode('utf8'))
+		self.assertTrue(response.content.endswith(b'</html>'))
